6dbe1ab76984a1d16dc11e579ada95775f4dcbc0
James Mead <james@renegade.net.nz>
2023-03-19 14:58:05 +1300

    testing changes

--- demo-data.py
+++ demo-data.py
@@ -4,7 +4,7 @@ import json
 kinesis = boto3.client('kinesis')
 
 stream_name = 'demo-kinesis-data-stream'
-partition_key = 'test-data'
+partition_key = 'test-data5'
 message = 'Hello, Kinesis!'
 
 response = kinesis.put_record(
